4c1495f3c38ab7a24aea8e9d101a5384c4dc4668
dengyuanke <dengyuanke@gmail.com>
2018-11-15 18:26:17 +0800

    Eureka服务器

---
+++ weather-eureka-service/src/main/java/com/dengyuanke/eurekaservice/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.dengyuanke.eurekaservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: dyk
+ * Create time: 2018/11/15  18:20
+ */
+@RestController
+public class Controller {
+}
